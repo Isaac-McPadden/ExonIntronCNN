b776b8483b2f55d7fedd8d5b71f75bda28282990
Isaac McPadden <imcpadden523@gmail.com>
2025-04-16 22:18:19 -0600

    Started the process of making a config file to handle project paths better

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+import os
+
+# Project base directory (absolute path)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Subdirectories
+DATA_DIR = os.path.join(BASE_DIR, "Datasets")
+LOG_DIR = os.path.join(BASE_DIR, "Logs")
+MODEL_DIR = os.path.join(BASE_DIR, "Models")
+MODULE_DIR = os.path.join(BASE_DIR, "IEMODULES")
