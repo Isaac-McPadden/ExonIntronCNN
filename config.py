0e951cbe7f084f9ff62d730127e36beeb18bc9b0
Isaac McPadden <imcpadden523@gmail.com>
2025-04-17 22:54:24 -0600

    Fixed and updated config.py

--- config.py
+++ config.py
@@ -8,4 +8,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "Datasets")
 LOG_DIR = os.path.join(BASE_DIR, "Logs")
 MODEL_DIR = os.path.join(BASE_DIR, "Models")
-MODULE_DIR = os.path.join(BASE_DIR, "IEMODULES")
+MODULE_DIR = os.path.join(BASE_DIR, "IEModules")
+NOTEBOOKS_DIR = os.path.join(BASE_DIR, "Notebooks") # Probably unnecessary
